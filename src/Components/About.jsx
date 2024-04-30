ff1f969b810244449a4482df827f687632e00eab
Flavio Meyer <flavio.meyer@flavio-meyer.ch>
2024-04-30 21:37:01 +0200

    Update about-me

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -24,19 +24,19 @@ const imageAltText = "purple and blue abstract background";
  * Sort description that expands on your title on the Home component.
  */
 const description =
-  "I'm a UI/UX student studying at Barnett Technical University. I enjoy creating unique and simplistic user interfaces in creative ways.";
+  "I'm a information technoligy student studying at IPSO Bildung AG. I enjoy working together with Microsoft Azure.";
 
 /**
  * List of some of skills or technologies you work on, are learning,
  * passionate about, or enjoy,
  */
 const skillsList = [
-  "Web design",
-  "User experience",
-  "Inclusive design",
-  "Focus group testing",
-  "Mobile user interfaces",
-  "Graphic design",
+  "Microsoft Azure",
+  "Microsoft 365",
+  "Windows Server and Client OS",
+  "CI/CD Pipelines",
+  "Infrastructure as Code",
+  "Linux automation and scripting",
 ];
 
 /**
@@ -45,7 +45,7 @@ const skillsList = [
  * about you on a professional level.
  */
 const detailOrQuote =
-  "I am passionate about solving problems in new creative ways to drive innovation. By leveraging my UI/UX experience I continually look for new and better ways to make tech accessible by all.";
+  "I am passionate about building new and highly scalabale solutions within Microsoft Azure. I continually look for new and better ways to make tech accessible by all.";
 
 const About = () => {
   return (
--- src/Components/Portfolio.jsx
+++ src/Components/Portfolio.jsx
@@ -31,28 +31,28 @@ const imageAltText = "desktop with books and laptop";
  */
 const projectList = [
   {
-    title: "10 Things To Know About Azure Static Web Apps 🎉",
+    title: "Azure CIS – Powerpipe",
     description:
-      "Collaboration to create a beginner friendly article to help explain Azure Static Web Apps and tooling to get started.",
-    url: "https://dev.to/azure/10-things-to-know-about-azure-static-web-apps-3n4i",
+      "Guide about how to use Powerpipe for Azure CIS.",
+    url: "https://duo-infernale.ch/azure-cis-powerpipe/",
   },
   {
-    title: "Web Development for Beginners",
+    title: "Azure CIS – Turbot Pipes",
     description:
-      "Contributed sketch note imagery to accompany each lesson. These help provide visual representation of what is being taught.",
-    url: "https://github.com/microsoft/web-dev-for-beginners",
+      "How-to guide for using Turbot Pipes for Azure CIS.",
+    url: "https://duo-infernale.ch/azure-cis-turbot-pipes/",
   },
   {
-    title: "My Resume Site",
+    title: "Azure Security Benchmark",
     description:
-      "Created from Microsoft's resume workshop and deployed to GitHub pages. Includes my experience and design abilities.",
-    url: "https://github.com/microsoft/workshop-library/tree/main/full/build-resume-website",
+      "Deep-dive into Azure Security Benchmark.",
+    url: "https://duo-infernale.ch/azure-security-benchmark/",
   },
   {
-    title: "GitHub Codespaces and github.dev",
+    title: "Conditional Access Policy – Insider risk (Preview)",
     description:
-      "Video interview to explain when to use GitHub.dev versus GitHub Codespaces, and how best to use each tool.",
-    url: "https://www.youtube.com/watch?v=c3hHhRME_XI",
+      "Elaboration on Conditional Access Policy for Insider Risk Preview.",
+    url: "https://duo-infernale.ch/conditional-access-policy-insider-risk-preview/",
   },
 ];
 
